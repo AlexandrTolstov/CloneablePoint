d39a076b93869c0cca2643da1b375fa2e92c3999
Alexandr <tolstov.alexandr.85@gmail.com>
2019-06-18 11:59:54 +0600

    1

--- CloneablePoint/Point.cs
+++ CloneablePoint/Point.cs
@@ -10,10 +10,23 @@ namespace CloneablePoint
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public PointDescription desc = new PointDescription();
+        public Point(int xPos, int yPOs, string petName)
+        {
+            X = xPos; Y = yPOs;
+            desc.PetName = petName;
+        }
         public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
         public Point() { }
-        public override string ToString() => $"X = {X}; Y = {Y}";
+        public override string ToString() => $"X = {X}; Y = {Y} Name = {desc.PetName};\nID = {desc.PointID}\n";
+        public object Clone()
+        {
+            Point newPoint = (Point)this.MemberwiseClone();
 
-        public object Clone() => new Point(this.X, this.Y);
+            PointDescription currentDesc = new PointDescription();
+            currentDesc.PetName = this.desc.PetName;
+            newPoint.desc = currentDesc;
+            return newPoint;
+        }
     }
 }
